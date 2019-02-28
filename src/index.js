a2ef5434f3588b4b438085229ed688adf2bf72bf
Anton Yegorov <an_tosha33@mail.ru>
2019-02-28 19:03:36 +0300

    the second release

--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  	var is = [].slice.call(str);
-		for(var i = 0, d = 0; i < is.length; i++){
-			if(isOpened(is[i])) continue;
-			for(var j = 0; j < bracketsConfig.length; j++ ){
-				if(is[i]==bracketsConfig[j][1]){
-					if(is[i-1]==bracketsConfig[j][0]){
-						is.splice(i-1,2);
-						i=-1;
-						break;
-					}
-				}
-			}
-		}
-		function isOpened(a){
-			for(var j = 0; j < bracketsConfig.length; j++ ){
-				if(a == bracketsConfig[j][0]){
-					return true;
-				}else if(a == '|' || a == '7' || a == '8'){
-					return false;
-				}
-			}
-		}
-		if(is.length != 0){
-			for(var i = 0; i < is.length; i++){
-				if(is[i]=='|' && is[i+1]=='|')return true;
-				if(is[i] == '7' && is[i+1] == '7')return true;
-				if(is[i] == '8' && is[i+1] == '8')return true;
-			}
-		}
-		return is.length==0 ? true : false;
-}
+        var is = [].slice.call(str);
+          for(var i = 0, d = 0; i < is.length; i++){
+              if(isOpened(is[i])==false) continue;
+              for(var j = 0; j < bracketsConfig.length; j++ ){
+                  if(is[i]==bracketsConfig[j][1]){
+                     if(is[i-1]==bracketsConfig[j][0]){
+                          is.splice(i-1,2);
+                          i=-1;
+                          break;
+                      }
+                  }
+              }
+          }
+          function isOpened(a){
+              for(var j = 0; j < bracketsConfig.length; j++ ){
+                  if(a == bracketsConfig[j][0] && bracketsConfig[j][0] == bracketsConfig[j][1]){
+                      return true;
+                  }else if(a == bracketsConfig[j][0]){
+                      return false;
+                  }
+              }
+          }
+        return is.length==0 ? true : false;
+  }
+
